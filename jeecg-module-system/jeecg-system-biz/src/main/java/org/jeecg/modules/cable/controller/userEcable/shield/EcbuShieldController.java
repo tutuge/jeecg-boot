a7c3a6ae7f811fa0189129c900d8b86b7a55d10a
tutuge <850083043@qq.com>
2023-10-16 22:52:58 +0800

    屏蔽

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cable/controller/userEcable/shield/EcbuShieldController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cable/controller/userEcable/shield/EcbuShieldController.java
@@ -1,15 +1,20 @@
-package org.jeecg.modules.cable.controller.userEcable;
+package org.jeecg.modules.cable.controller.userEcable.shield;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
+import org.jeecg.common.api.vo.Result;
+import org.jeecg.modules.cable.controller.userEcable.shield.bo.EcbuShieldBo;
+import org.jeecg.modules.cable.controller.userEcable.shield.bo.EcbuShieldListBo;
+import org.jeecg.modules.cable.controller.userEcable.shield.bo.EcbuShieldStartBo;
+import org.jeecg.modules.cable.entity.userEcable.EcbuShield;
 import org.jeecg.modules.cable.model.user.EcuLoginModel;
 import org.jeecg.modules.cable.model.userEcable.EcbuShieldModel;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.List;
 
 @Tag(name = "屏蔽")
 @RestController
@@ -22,28 +27,23 @@ public class EcbuShieldController {
     @Operation(summary = "编辑屏蔽")
     //deal
     @PostMapping({"/ecableErpPc/ecbuShield/deal"})
-    public Map<String, Object> login_deal(HttpServletRequest request) {
-
-            map = ecbuShieldModel.deal(request);
-        return map;
+    public Result<?> login_deal(@RequestBody EcbuShieldBo bo) {
+        ecbuShieldModel.deal(bo);
+        return Result.ok();
     }
 
     @Operation(summary = "开启屏蔽")
     //start
     @PostMapping({"/ecableErpPc/ecbuShield/start"})
-    public Map<String, Object> start(HttpServletRequest request) {
-
-            map = ecbuShieldModel.start(request);
-        return map;
+    public Result<String> start(@RequestBody EcbuShieldStartBo bo) {
+        return Result.ok(ecbuShieldModel.start(bo));
     }
 
 
     @Operation(summary = "屏蔽列表")
     //getList
     @PostMapping({"/ecableErpPc/ecbuShield/getList"})
-    public Map<String, Object> getList(HttpServletRequest request) {
-
-            map = ecbuShieldModel.getList(request);
-        return map;
+    public Result<List<EcbuShield>> getList(@RequestBody EcbuShieldListBo bo) {
+        return Result.ok(ecbuShieldModel.getList(bo));
     }
 }
--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cable/model/userEcable/EcbuShieldModel.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cable/model/userEcable/EcbuShieldModel.java
@@ -1,21 +1,22 @@
 package org.jeecg.modules.cable.model.userEcable;
 
-import org.jeecg.modules.cable.entity.systemEcable.EcbShield;
+import jakarta.annotation.Resource;
+import org.apache.shiro.SecurityUtils;
 import org.jeecg.common.system.vo.EcUser;
+import org.jeecg.common.system.vo.LoginUser;
+import org.jeecg.modules.cable.controller.userEcable.shield.bo.EcbuShieldBo;
+import org.jeecg.modules.cable.controller.userEcable.shield.bo.EcbuShieldListBo;
+import org.jeecg.modules.cable.controller.userEcable.shield.bo.EcbuShieldStartBo;
+import org.jeecg.modules.cable.entity.systemEcable.EcbShield;
 import org.jeecg.modules.cable.entity.userEcable.EcbuShield;
 import org.jeecg.modules.cable.model.systemEcable.EcbShieldModel;
 import org.jeecg.modules.cable.service.systemEcable.EcbShieldService;
 import org.jeecg.modules.cable.service.user.EcUserService;
 import org.jeecg.modules.cable.service.userEcable.EcbuShieldService;
-import org.jeecg.modules.cable.tools.CommonFunction;
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class EcbuShieldModel {
@@ -29,28 +30,15 @@ public class EcbuShieldModel {
     EcbShieldModel ecbShieldModel;
 
     //deal
-    public Map<String, Object> deal(HttpServletRequest request) {
-        Map<String, Object> map = new HashMap<>();
-        int status;
-        String code;
-        String msg;
-        int ecuId = Integer.parseInt(request.getParameter("ecuId"));
-        EcUser recordEcUser = new EcUser();
-        recordEcUser.setEcuId(ecuId);
-        EcUser ecUser = ecUserService.getObject(recordEcUser);
-        int ecbsId = Integer.parseInt(request.getParameter("ecbsId"));
-        BigDecimal unitPrice = new BigDecimal("0");
-        if (request.getParameter("unitPrice") != null) {
-            unitPrice = new BigDecimal(request.getParameter("unitPrice"));//单价
-        }
-        BigDecimal density = new BigDecimal("0");
-        if (request.getParameter("density") != null) {
-            density = new BigDecimal(request.getParameter("density"));//密度
-        }
-        String description = "";
-        if (request.getParameter("description") != null) {
-            description = request.getParameter("description");
-        }
+    public void deal(EcbuShieldBo bo) {
+        BigDecimal unitPrice = bo.getUnitPrice();
+        BigDecimal density = bo.getDensity();
+        String description = bo.getDescription();
+        //获取当前用户id
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+        EcUser ecUser = sysUser.getEcUser();
+        Integer ecbsId = bo.getEcbsId();
+
         EcbuShield record = new EcbuShield();
         record.setEcbsId(ecbsId);
         record.setEcCompanyId(ecUser.getEcCompanyId());
@@ -62,46 +50,30 @@ public class EcbuShieldModel {
             record.setDensity(density);
             record.setDescription(description);
             ecbuShieldService.insert(record);
-            status = 3;//插入
-            code = "200";
-            msg = "插入数据";
         } else {
             record.setEcbusId(ecbuShield.getEcbusId());
-            if (request.getParameter("unitPrice") != null) {
-                record.setUnitPrice(unitPrice);
-            }
-            if (request.getParameter("density") != null) {
-                record.setDensity(density);
-            }
-            if (request.getParameter("description") != null) {
-                record.setDescription(description);
-            }
+            record.setUnitPrice(unitPrice);
+            record.setDensity(density);
+            record.setDescription(description);
             ecbuShieldService.update(record);
-            status = 4;//更新数据
-            code = "201";
-            msg = "更新数据";
         }
-        CommonFunction.getCommonMap(map, status, code, msg);
-        ecbShieldModel.loadData(request);//txt文档
-        return map;
     }
 
     //start
-    public Map<String, Object> start(HttpServletRequest request) {
-        Map<String, Object> map = new HashMap<>();
-        int status;
-        String code;
-        String msg;
-        int ecuId = Integer.parseInt(request.getParameter("ecuId"));
-        EcUser recordEcUser = new EcUser();
-        recordEcUser.setEcuId(ecuId);
-        EcUser ecUser = ecUserService.getObject(recordEcUser);
-        int ecbsId = Integer.parseInt(request.getParameter("ecbsId"));
+    public String start(EcbuShieldStartBo bo) {
+
+        Integer ecbsId = bo.getEcbsId();
         EcbuShield record = new EcbuShield();
         record.setEcbsId(ecbsId);
+
+        //获取当前用户id
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+        EcUser ecUser = sysUser.getEcUser();
         record.setEcCompanyId(ecUser.getEcCompanyId());
         EcbuShield ecbuShield = ecbuShieldService.getObject(record);
         boolean startType;
+
+        String msg = "";
         if (ecbuShield == null) {//插入数据
             EcbShield recordEcbShield = new EcbShield();
             recordEcbShield.setEcbsId(ecbsId);
@@ -114,20 +86,15 @@ public class EcbuShieldModel {
             record.setDensity(ecbShield.getDensity());
             record.setDescription("");
             ecbuShieldService.insert(record);
-            status = 3;//启用成功
-            code = "200";
+
             msg = "数据启用成功";
         } else {
             startType = ecbuShield.getStartType();
             if (!startType) {
                 startType = true;
-                status = 3;
-                code = "200";
                 msg = "数据启用成功";
             } else {
                 startType = false;
-                status = 4;
-                code = "201";
                 msg = "数据禁用成功";
             }
             record.setEcbusId(ecbuShield.getEcbusId());
@@ -135,27 +102,18 @@ public class EcbuShieldModel {
             //log.info(CommonFunction.getGson().toJson(record));
             ecbuShieldService.update(record);
         }
-        CommonFunction.getCommonMap(map, status, code, msg);
-        ecbShieldModel.loadData(request);//txt文档
-        return map;
+        ecbShieldModel.loadData();//txt文档
+        return msg;
     }
 
     //getList
-    public Map<String, Object> getList(HttpServletRequest request) {
-LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+    public List<EcbuShield> getList(EcbuShieldListBo bo) {
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
         EcUser ecUser = sysUser.getEcUser();
         EcbuShield record = new EcbuShield();
         record.setEcCompanyId(ecUser.getEcCompanyId());
-        if ("1".equals(startType)) {
-            record.setStartType(true);
-        }
-        List<EcbuShield> list = ecbuShieldService.getList(record);
-        map.put("list", list);
-        status = 3;
-        code = "200";
-        msg = "正常获取数据";
-        CommonFunction.getCommonMap(map, status, code, msg);
-        return map;
+        record.setStartType(bo.getStartType());
+        return ecbuShieldService.getList(record);
     }
 
     /***===数据模型===***/
